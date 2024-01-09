8f23547f929827292641b5d757ec9ea5b15127ba
Mehmet Göktuğ Aslan <Prometikios@users.noreply.github.com>
2024-01-09 18:14:12 +0100

    Update WebScarp1.py

--- ForTester/WebScarp1.py
+++ ForTester/WebScarp1.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import subprocess
-import json
+import json, shutil
 
 IngType = {
     "SPOON" : 'yemek kaşığı ',
